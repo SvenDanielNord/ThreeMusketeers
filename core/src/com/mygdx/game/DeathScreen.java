c356f195dadd51327d674384e12f21e2c4265cd4
Daniel Nord <svenor0701@skola.goteborg.se>
2023-02-16 12:15:22 +0100

    added backgroundimage

--- core/src/com/mygdx/game/DeathScreen.java
+++ core/src/com/mygdx/game/DeathScreen.java
@@ -1,5 +1,81 @@
 package com.mygdx.game;
 
-public class DeathScreen {
-    
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+public class DeathScreen implements Screen {
+
+    final FlappyBird game;
+
+    OrthographicCamera camera;
+
+    Texture background;
+
+    public DeathScreen(FlappyBird game) {
+        this.game = game;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+        background = new Texture(Gdx.files.internal("bgtest.png"));
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        ScreenUtils.clear(0,0,0,1);
+        camera.update();
+
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+        game.batch.draw(background,0,0);
+        game.batch.end();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            game.setScreen((new MainMenuScreen(game)));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
