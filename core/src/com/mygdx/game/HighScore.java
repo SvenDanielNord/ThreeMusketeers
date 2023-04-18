350f51ef60f5a0136fb74556bc137072a3216d3d
SvenDanielNord <svenor0701@skola.goteborg.se>
2023-04-18 13:24:31 +0200

    Add highscore for each level.

--- core/src/com/mygdx/game/HighScore.java
+++ core/src/com/mygdx/game/HighScore.java
@@ -1,33 +1,69 @@
 package com.mygdx.game;
 
+import com.mygdx.game.utils.Levels;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 
-    public class HighScore {
+public class HighScore {
+
+    private static String highScorePath;
+    static int highScoreEasy;
+    static int highScoreHard;
+    static int highScoreMedium;
     static int highScore;
     static int allTimeScore;
 
-    static int setHighScore(int score) {
-        //saving high score if it's bigger than score
-        if (score > highScore) {
-            highScore = score;
-        }
+    static int setHighScore(int score, Levels level) {
 
-        readAllTimeScore(highScore);
+        if (level == Levels.EASY){
+            if (score > highScoreEasy) {
+                highScoreEasy = score;
+                highScore = highScoreEasy;
+                readAllTimeScore(highScoreEasy);
+            }
+            highScore = highScoreEasy;
+        }else if (level == Levels.HARD){
+            if (score > highScoreHard) {
+                highScoreHard = score;
+                highScore = highScoreHard;
+                readAllTimeScore(highScoreHard);
+            }
+            highScore = highScoreHard;
+        }else {
+            if (score > highScoreMedium) {
+                highScoreMedium = score;
+                highScore = highScoreMedium;
+                readAllTimeScore(highScoreMedium);
+            }
+            highScore = highScoreMedium;
+        }
+        //saving high score if it's bigger than score
+
+
+
+
         //writing all-time high score to a file
 
 
-        try {
-            FileWriter writer = new FileWriter("highscore.txt");
-            writer.write(Integer.toString(allTimeScore));
-            writer.close();
-        } catch (IOException ex) {
-            System.out.println("Something went wrong: " + ex.getMessage());
-        }
+
         return highScore;
+    }
+
+
+
+
+    static void separateHighscores(Levels level) {
+        if (level == Levels.EASY) {
+            highScorePath = "easy.txt";
+        } else if (level == Levels.HARD) {
+            highScorePath = "hard.txt";
+        } else {
+            highScorePath = "highscore.txt";
+        }
     }
 
     static void readAllTimeScore(int highScore) {
@@ -36,7 +72,7 @@ import java.util.Scanner;
         //writing and reading all-time high score to/from a file
         try {
 
-            Scanner scanner = new Scanner(new File("highscore.txt"));
+            Scanner scanner = new Scanner(new File(highScorePath));
             while (scanner.hasNextLine()) {
                 allTimeScore = Integer.parseInt(scanner.nextLine());
 
@@ -47,8 +83,18 @@ import java.util.Scanner;
         }
         if (highScore > allTimeScore) {
             allTimeScore = highScore;
+            writeAlltimeHighScore();
         }
 
+    }
+    private static void writeAlltimeHighScore() {
+        try {
+            FileWriter writer = new FileWriter(highScorePath);
+            writer.write(Integer.toString(allTimeScore));
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println("Something went wrong: " + ex.getMessage());
+        }
     }
 
 }
